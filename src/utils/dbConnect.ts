1c7460fa506df1a26ec3cf0c04bd9ec4f4444d86
emirace <emmanuelakwuba57@gmail.com>
2025-03-09 12:37:13 +0100

    up

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,20 +1,42 @@
 import mongoose from "mongoose";
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
 
-const MONGO_URI = process.env.MONGO_URI as string;
+let cached = global.mongoose;
 
-if (!MONGO_URI) {
-  throw new Error(
-    "Please define the MONGO_URI environment variable inside .env.local"
-  );
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log("db  connected");
-  } catch (error) {
-    console.log(error);
+  const MONGO_URI = process.env.MONGO_URI!;
+
+  if (!MONGO_URI) {
+    throw new Error(
+      "Please define the MONGO_URI environment variable inside .env.local"
+    );
   }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 export default dbConnect;
